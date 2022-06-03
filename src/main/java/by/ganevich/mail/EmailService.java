eddacb6d3468d71dfa9ce5687b93dbf628df88ad
Matsvei Hanevich <matsvei.hanevich@intexsoft.by>
2022-06-03 12:05:11 +0300

    Add flag to disable email sender

--- src/main/java/by/ganevich/mail/EmailService.java
+++ src/main/java/by/ganevich/mail/EmailService.java
@@ -1,7 +1,10 @@
 package by.ganevich.mail;
 
 import by.ganevich.dto.RegistrationRequestDto;
+import by.ganevich.entity.User;
+import by.ganevich.entity.enums.State;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -21,6 +24,9 @@ public class EmailService {
     private final TemplateEngine templateEngine;
     private final JavaMailSender javaMailSender;
     private final String subject = "BankSystem security verification";
+
+    @Value("${isEmailSenderActive}")
+    private String isEmailSenderActive;
 
     private static String getRandomNumberString() {
         Random rnd = new Random();
@@ -48,6 +54,14 @@ public class EmailService {
         helper.setTo(user.getLogin());
         javaMailSender.send(mimeMessage);
         return number;
+    }
+
+    public void invokeEmailSender(User user, RegistrationRequestDto registrationRequest) throws MessagingException {
+        if (isEmailSenderActive.equals(true)) {
+            user.setCode(sendEmail(registrationRequest));
+        } else {
+            user.setState(State.ACTIVATED);
+        }
     }
 
 }
